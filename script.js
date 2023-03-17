adcd95442ecd07464529c1abc8dde0feff6e2bb0
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-03-17 20:52:09 +0600

    updated

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 let images = ["images/pic-1.jpg", "images/pic-2.jpg", "images/pic-3.jpg"];
 const imageSlider = document.getElementById("img-slider");
 
-// let imgIndex = 0;
-// setInterval(() => {
-//   if (imgIndex === images.length) {
-//     imgIndex = 0;
-//   }
-//   const imgUrl = images[imgIndex];
-//   console.log(imgUrl);
-//   imageSlider.setAttribute("src", imgUrl);
-//   imgIndex++;
-// }, 4000);
-
 let imageIndex = 0;
-
 setInterval(() => {
   if (imageIndex === images.length) {
     imageIndex = 0;
